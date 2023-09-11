97932efb94552339b6cbe22c85e9392401948526
Senna Oosterink <60702315+SennaOosterink@users.noreply.github.com>
2023-09-11 15:41:48 +0200

    Update home.blade.php

--- SmartCam Website/resources/views/home.blade.php
+++ SmartCam Website/resources/views/home.blade.php
@@ -2,8 +2,26 @@
 <html>
     <head>
         <title>Bewakingscamera</title>
-        <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="styles.css">
     </head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        nav {
+            width: 100%;
+            height: 70px;
+            background-color: rgb(240, 240, 240);
+            line-height: 70px;
+            position: fixed;
+        }
+        h1 {
+            color: rgb(20, 20, 200);
+            text-align: center;
+        }
+    </style>
     <body>
         <nav>
             <h1>Dashboard</h1>
