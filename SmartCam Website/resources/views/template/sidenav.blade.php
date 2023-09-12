77745345b4ebe48cc726555293a5c5f5cfecd05c
Senna Oosterink <60702315+SennaOosterink@users.noreply.github.com>
2023-09-12 11:45:39 +0200

    senna

--- SmartCam Website/resources/views/template/sidenav.blade.php
+++ SmartCam Website/resources/views/template/sidenav.blade.php
@@ -1,6 +1,6 @@
 <div id="sidenav" class="sidenav">
     <p class="events">Gebeurtenissen</p>
-    <a href="home.blade.php"></a>
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     @foreach($events as $event)
     <div class="notification">
         <p>datum:</p>
@@ -11,3 +11,13 @@
         <button class="stopBtn">Stop Detectie</button>
     </div>
 </div>
+
+<script>
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
+</script>
