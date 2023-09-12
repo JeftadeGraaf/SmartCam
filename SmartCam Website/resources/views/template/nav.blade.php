6979588d18a01482fb457fa7196e8009614e49c3
Senna Oosterink <60702315+SennaOosterink@users.noreply.github.com>
2023-09-12 12:48:53 +0200

    Senna

--- SmartCam Website/resources/views/template/nav.blade.php
+++ SmartCam Website/resources/views/template/nav.blade.php
@@ -1,3 +1,4 @@
 <nav>
-    <a href="http://127.0.0.1:8000"><p class="dashboard">Bewakings Dashboard</p></a>
+    <a class="dashboardTitle" href="http://127.0.0.1:8000"><p class="dashboard">Bewakings Dashboard</p></a>
+</svg>
 </nav>
