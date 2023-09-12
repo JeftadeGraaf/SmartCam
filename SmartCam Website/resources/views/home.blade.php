257cc9379ef1ed8b6dd815493a361dba6975fef5
Senna Oosterink <60702315+SennaOosterink@users.noreply.github.com>
2023-09-12 12:02:49 +0200

    Senna

--- SmartCam Website/resources/views/home.blade.php
+++ SmartCam Website/resources/views/home.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Bewakingscamera</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
-        <link href="{{ asset('app.css') }}" rel="stylesheet">
+        @include('template.headTag')
     </head>
     <style>
 
---
+++ SmartCam Website/resources/views/template/headTag.blade.php
@@ -0,0 +1,8 @@
+<title>Bewakingscamera</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+<link href="{{ asset('app.css') }}" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Poppins:wght@200&display=swap" rel="stylesheet">
