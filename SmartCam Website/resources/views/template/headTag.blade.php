0df1b4c6418936c48cc88d76fbff1fb8c2198321
Senna Oosterink <60702315+SennaOosterink@users.noreply.github.com>
2023-09-12 12:08:50 +0200

    Senna

--- SmartCam Website/resources/views/template/headTag.blade.php
+++ SmartCam Website/resources/views/template/headTag.blade.php
@@ -5,4 +5,4 @@
 <link href="{{ asset('app.css') }}" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com"> 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Poppins:wght@200&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Poppins:wght@300&display=swap" rel="stylesheet">
--- SmartCam Website/resources/views/template/nav.blade.php
+++ SmartCam Website/resources/views/template/nav.blade.php
@@ -1,3 +1,3 @@
 <nav>
-    <a href="http://127.0.0.1:8000"><h1>Dashboard</h1></a>
+    <a href="http://127.0.0.1:8000"><p class="dashboard">Bewakings Dashboard</p></a>
 </nav>
